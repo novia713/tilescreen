6095f7fce59914ede73890b4eb52a615ea8fc7b0
leandro713 <leandro@leandro.org>
2016-01-21 12:55:52 +0100

    v1.3 corrected get_color() [more vivid colors] 2

--- js/apps.js
+++ js/apps.js
@@ -79,11 +79,7 @@ require(['ramdajs'], ( R ) => {
             return obj_color[app];
         } else {
             //random hex color;
-
             return '#'+'0123456789abcdef'.split('').map(function(v,i,a){ return i>5 ? null : a[Math.floor(Math.random()*16)] }).join('');
-
-
-
         }
     };
 
